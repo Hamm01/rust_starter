b52867e5eb49b58132b42f6453d4b902d912f123
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-01 11:02:51 +0530

    Added numbers as signed or unsigned

---
+++ src/numbers.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x: i32 = -23;   // this will used for postive or negative, last bit is reserved for saving post or negtive value
+    println!("{}", x);
+}
+
+// u32 hold unsigned integer value, the last bit will also used to store the whole number , not for postive or negative 
+// 8, 32, 64, 128, these are all sizes in that can store numbers as acoording to space,
