283f50013765c932c69020541feeaa22e0f03f1b
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-03 02:34:19 +0530

    Vectors declarations

--- src/Arrays.rs
+++ src/Arrays.rs
@@ -4,3 +4,15 @@ fn main(){
   println("{}",arr.len());
   println("{}",arr);
 }
+
+//Vectors declaration
+
+fn main() {
+    let mut xs = vec![1, 2, 3];
+    
+    print!("{}", xs.len());
+
+    xs.push(4);
+    
+    print!("{}", xs.len());
+}
