2054608e8eb73a34c87641e58cf8eb32b334e611
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-02 09:01:06 +0530

    Booleans functions

--- src/numbers.rs
+++ src/numbers.rs
@@ -5,3 +5,19 @@ fn main() {
 
 // u32 hold unsigned integer value, the last bit will also used to store the whole number , not for postive or negative 
 // 8, 32, 64, 128, these are all sizes in that can store numbers as acoording to space,
+
+fn main() {
+    let is_male = false;
+    let is_above_18 = true;
+    // how booleans arre stored
+    if is_male {
+        println!("You are a male");
+
+    } else {
+        println!("You are not a male");
+    }
+
+    if is_male && is_above_18 {
+        print!("You are a legal male");
+    }
+}
