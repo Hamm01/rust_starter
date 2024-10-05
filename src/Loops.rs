8c32f2987dbf39af245a024f59b6dc6b7e00fcf7
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-05 12:30:30 +0530

    Loop for string , to get the first name

--- src/Loops.rs
+++ src/Loops.rs
@@ -3,4 +3,23 @@ fn main(){
     for i in 0..100{
     println!("{}",i);
   }
-  }
+}
+
+fn main(){
+  // Loop for string
+    let str = String::from("Himanish kochhar");
+    println!("firstname {}", get_first_name(str));
+
+}
+
+pub fn get_first_name(str: String) -> String{
+       let mut firstname = String::from("");
+
+       for c in str.chars(){
+           if c == ' '{
+          break }
+          firstname.push(c);
+       }
+       return firstname;
+
+}
