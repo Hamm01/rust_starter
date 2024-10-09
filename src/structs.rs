73bdd267fcf2e61217a2d20be8f720da9face91d
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-09 08:54:26 +0530

    declaring the struct

---
+++ src/structs.rs
@@ -0,0 +1,16 @@
+// structs like object in javascript that will need to structure the data together
+
+
+struct User{
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    // how to declare the struct
+}
+
+
+fn main(){
+
+
+}
