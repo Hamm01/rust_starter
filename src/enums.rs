9f720fe3c9c98c09aa071cd5235619f9a1d1cfc3
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-12 03:20:16 +0530

    Pattern matching using enums

--- src/enums.rs
+++ src/enums.rs
@@ -1,6 +1,8 @@
 // Enums are seemingly similar to enum in rust
 // how to define an enum
 
+// Pattern matching using enums
+
 enum Color{
     Red,
     Yellow,
@@ -10,27 +12,18 @@ enum Color{
   }
   
   fn main() {
-    let newColor = Color::White;
+    let newColor = Color::Red;
     refinecolor(newColor);
-
+  
   }
   
-  fn refinecolor(new:Color){
+  fn refinecolor(new : Color){
+  match new{
+    // Pattern matching using enums
 
-    // This is not working code but to take an idea how its works, 
-     if(new == Color::White){
-       println!("Color to refine is white");
-      }
-      if(new == Color::Black){
-        println!("Color to refine is Black");
-       }
-      if(new == Color::Red){
-        println!("Color to refine is Red");
-       }
-       if(new == Color::Yellow){
-        println!("Color to refine is Yellow");
-       }
-       if(new == Color::Green){
-        println!("Color to refine is Green");
-       }
+       Color::Black => print!("Black color to refine"),
+       Color::White => print!("White color to refine"),
+       _ => print!("Colors other than white and black")
+  }
+  
   }
