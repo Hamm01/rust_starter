c6552c2d2b0ea5667d587da04437e0bf4f48fab3
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-11 12:14:03 +0530

    Enums used in funciton

--- src/enums.rs
+++ src/enums.rs
@@ -1,11 +1,36 @@
 // Enums are seemingly similar to enum in rust
 // how to define an enum
 
-
 enum Color{
     Red,
     Yellow,
     White,
     Black,
     Green,
-}
+  }
+  
+  fn main() {
+    let newColor = Color::White;
+    refinecolor(newColor);
+
+  }
+  
+  fn refinecolor(new:Color){
+
+    // This is not working code but to take an idea how its works, 
+     if(new == Color::White){
+       println!("Color to refine is white");
+      }
+      if(new == Color::Black){
+        println!("Color to refine is Black");
+       }
+      if(new == Color::Red){
+        println!("Color to refine is Red");
+       }
+       if(new == Color::Yellow){
+        println!("Color to refine is Yellow");
+       }
+       if(new == Color::Green){
+        println!("Color to refine is Green");
+       }
+  }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 fn main(){
-
     let greeting = String::from("Himanish on way to solana hypes!!");
     println!("{}",greeting);
-  
   }
