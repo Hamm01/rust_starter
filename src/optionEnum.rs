a20c05a9a689721a8706c34a1c8d96c02bf86d6b
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-15 11:08:36 +0530

    optional enum used to find index of a in string

--- src/optionEnum.rs
+++ src/optionEnum.rs
@@ -12,3 +12,25 @@
 
 // For a function that will return null , we return option enum instead
 
+fn main(){
+  let result =  find_first_a(String::from("Himanish"));
+  // we will find the first occurence of a in string, and in case string not have a then the function will pass none instead of null
+  match result{
+      None => print!("Value not found!"),
+      Some(val) => print!("a  is at index {}",val)
+  }
+}
+
+
+fn find_first_a(str: String)  -> Option<u32>{
+
+  let mut index:u32 = 0;
+  for c in str.chars(){
+      if c == 'a'{
+         return Some(index)
+      }
+      index = index +1;
+  }
+ return None;
+
+}
