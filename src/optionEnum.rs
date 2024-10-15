b2e602f834ead7f5f532c4fd0afdbf58fa95fff9
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-15 09:45:21 +0530

    option enum concept of nullabilty

---
+++ src/optionEnum.rs
@@ -0,0 +1,14 @@
+/*
+
+  Optiomal enum in rust, this is introduced in rust to solve the concept of null values,, 
+  in rust we dont have the null values. 
+
+  pub enum Option<T>{
+  None,
+  Some(T),
+  }
+
+*/
+
+// For a function that will return null , we return option enum instead
+
