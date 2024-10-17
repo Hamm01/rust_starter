469fd433fcfe68a5468b3ee1b9c7c866a0f98df2
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-17 11:05:21 +0530

    local time using chrono

--- src/chrono.rs
+++ src/chrono.rs
@@ -1,5 +1,5 @@
 // We are using the Chrono library 
-use chrono::Utc;
+use chrono::{Utc,Local};
 
 /*
 we brought the extrernal crate to our project which is chrono , we added into cargo.toml file
@@ -11,5 +11,7 @@ chrono = "0.4.35"
 
 fn main(){
    let utc = Utc::now();
+   let local = Local::now(); // This will give us the local time 
    print!("{}",utc);
+   print!("{}",local);
 }
