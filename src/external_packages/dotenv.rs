6363b6d661b276236585350d30a7f0807654a10f
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-18 11:11:58 +0530

    Dotenv file added with to access env files

---
+++ src/external_packages/dotenv.rs
@@ -0,0 +1,16 @@
+/*
+
+Dotenv packages used in project to check the enviroment variables that
+need to be used in projects
+
+cargo add dotenv
+*/
+
+use dotenv::dotenv;
+use std::env;
+
+fn main() {
+    dotenv().ok();
+    let var = env::var("JWT_SECRET").unwrap();
+    println!("{}", var);
+}
