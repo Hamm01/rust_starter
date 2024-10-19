40bc9d2de79068ee2b5fea1280b6ecf35226f107
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-19 13:56:23 +0530

    unwrap and dotenv result enum explained

--- src/external_packages/dotenv.rs
+++ src/external_packages/dotenv.rs
@@ -11,7 +11,17 @@ use std::env;
 
 fn main() {
     dotenv().ok();
-    let var = env::var("JWT_SECRET").unwrap();
+    let var = env::var("JWT_SECRET").unwrap(); // here var will be of String if the env variable exist otherwise it will throw error
+
+    match var {
+        Ok(str) => println!("{}", str),
+        Err(_e) => print!("Error while reading variable"),
+    }
+}
+
+fn main() {
+    dotenv().ok();
+    let var = env::var("JWT_SECRET"); // here var be of result enum type : Result<String, env::VarError>
 
     match var {
         Ok(str) => println!("{}", str),
