0f147bf1843595b356f405eb9dba99017497325e
Himanish kochhar <himanish.kochhar@gmail.com>
2024-10-19 10:04:49 +0530

    uuid added and hoe to use

---
+++ src/external_packages/uuid.rs
@@ -0,0 +1,13 @@
+use uuid::Uuid;
+/* For generating the uuid
+ Adding something in dependices in cargo.toml file to run the new_v4 function
+[dependencies]
+chrono = "0.4.32"
+dotenv = "0.13.0"
+uuid = { version = "1.4", features = ["v4"] }
+
+*/
+fn main() {
+    let id = Uuid::new_v4();
+    println!("{}", id);
+}
